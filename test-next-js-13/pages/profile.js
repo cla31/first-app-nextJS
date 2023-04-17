3de2473e20450214627be7636a0da09fc518c43e
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-17 21:36:12 +0200

    Routes dynamiques avec des paramètres de requête partie 2

--- test-next-js-13/pages/profile.js
+++ test-next-js-13/pages/profile.js
@@ -1,12 +1,29 @@
-import { Layout } from "@/components/layout"
+import { Layout } from '@/components/layout'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
+const NameLink = ({ nom }) => (
+  <li>
+    <Link legacyBehavior href={`/profile?nom=${nom}`}>
+      <a>{nom}</a>
+    </Link>
+  </li>
+)
 const Profile = () => {
+  const router = useRouter()
+  console.log(router.query)
   return (
     <Layout>
-         <h1>Profile</h1>
+      <h1>Profile</h1>
+      <ul>
+        <NameLink nom="John Doe" />
+        <NameLink nom="Paul" />
+        <NameLink nom="Jean" />
+      </ul>
+      <h2>Affichage dynamique des noms::::</h2>
+      <h1>{router.query.nom}</h1>
     </Layout>
-
-   )  
+  )
 }
 
 export default Profile
