c962d518be630476f835c9efea9314a434cc88a1
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-17 16:12:59 +0200

    Style sur la page active

--- test-next-js-13/components/header.js
+++ test-next-js-13/components/header.js
@@ -1,6 +1,9 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 const Header = () => {
+    const router = useRouter()
+    console.log(router);
     const styles = {
         header:{
             margin:20,
@@ -9,25 +12,30 @@ const Header = () => {
         },
         link:{
             margin:15
+        },
+        active:{
+            margin:15,
+            color:"blue"
         }
     }
   return(
     <div style={styles.header}>
         {/* comme on utilise pas l'attribut a pour faire ledes liens on met l'attribut passHref */}
         <Link href="/" passHref>
-            <span style={styles.link}>Home</span>
+            {/* On met une condition pour que le lien actif soit bleu */}
+            <span style={router.pathname === "/" ? styles.active : styles.link }>Home</span>
         </Link>
         <Link href="/blog" passHref>
-            <span style={styles.link}>Blog</span>
+            <span style={router.pathname === "/blog" ? styles.active : styles.link}>Blog</span>
         </Link>
         <Link href="/profile" passHref>
-            <span style={styles.link}>Profile</span>
+            <span style={router.pathname === "/profile" ? styles.active : styles.link}>Profile</span>
         </Link>
         <Link href="/blog/items" passHref>
-            <span style={styles.link}>Items</span>
+            <span style={router.pathname === "/blog/items" ? styles.active : styles.link}>Items</span>
         </Link>
         <Link href="/blog/categories" passHref>
-            <span style={styles.link}>Catégories</span>
+            <span style={router.pathname === "/blog/categories" ? styles.active : styles.link}>Catégories</span>
         </Link>         
     </div>
   )
