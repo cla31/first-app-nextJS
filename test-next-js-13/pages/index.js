4c45d4dba7e6702c036f60600a372054602ac305
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-17 11:52:41 +0200

    Mise à jour création du projet

---
+++ test-next-js-13/pages/index.js
@@ -0,0 +1,12 @@
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Home = () => {
+  return (
+    <div>Bonjour</div>
+   )  
+}
+
+export default Home
