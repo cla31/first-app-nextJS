6181f2aa0c25a7e20df69722c9654076322da547
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-17 19:06:10 +0200

    Refactoring de la page Blog

--- test-next-js-13/pages/blog.js
+++ test-next-js-13/pages/blog.js
@@ -2,34 +2,25 @@ import { Layout } from '@/components/layout'
 // Pour accéder à chacun des éléments
 import Link from 'next/link'
 
+const PostLink = ({ titre }) => (
+  <li>
+    <Link legacyBehavior href={`/blog/${titre}`}>
+      <a>{titre}</a>
+    </Link>
+  </li>
+)
+
 const Blog = () => {
   return (
     <Layout>
       <h1>Blog</h1>
       {/* Création d'une liste de billets par technos */}
       <ul>
-        <li>
-          {/* as définit le titre dynamiquement */}
-          {/* J'ai rajouté legacyBehavior sinon cela me renvoit une erreur */}
-          <Link legacyBehavior href="/blog/[titre]" as={'/blog/react'}>
-            <a>React</a>
-          </Link>
-        </li>
-        <li>
-          <Link legacyBehavior href="/blog/[titre]" as={'/blog/angular'}>
-            <a>Angular</a>
-          </Link>
-        </li>
-        <li>
-          <Link legacyBehavior href="/blog/[titre]" as={'/blog/vue'}>
-            <a>Vue JS</a>
-          </Link>
-        </li>
-        <li>
-          <Link legacyBehavior href="/blog/[titre]" as={'/blog/svelte'}>
-            <a>Svelte</a>
-          </Link>
-        </li>
+        <PostLink titre="React" />
+        <PostLink titre="Angular" />
+        <PostLink titre="Vue JS" />
+        <PostLink titre="Svelte" />
+        <PostLink titre="Apprendre Next JS" />
       </ul>
     </Layout>
   )
