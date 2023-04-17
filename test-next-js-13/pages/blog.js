ac860d4e31628910a18f5f1e7bc30f49562510e7
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-17 19:35:40 +0200

    Routes dynamiques avec des paramètres de requêtes, partie 1

--- test-next-js-13/pages/blog.js
+++ test-next-js-13/pages/blog.js
@@ -1,27 +1,38 @@
-import { Layout } from '@/components/layout'
-// Pour accéder à chacun des éléments
+import { Layout } from '../components/layout'
 import Link from 'next/link'
-
+import { useRouter } from 'next/router'
+// const PostLink = ({ titre }) => (
+//   <li>
+//     <Link legacyBehavior href={`/blog/${titre}`}>
+//       <a>{titre}</a>
+//     </Link>
+//   </li>
+// )
+// Routes dynamiques avec des paramètres de requêtes
 const PostLink = ({ titre }) => (
   <li>
-    <Link legacyBehavior href={`/blog/${titre}`}>
+    <Link legacyBehavior href={`/blog?titre=${titre}`}>
       <a>{titre}</a>
     </Link>
   </li>
 )
 
 const Blog = () => {
+  const router = useRouter()
+  console.log(router.query)
   return (
     <Layout>
       <h1>Blog</h1>
       {/* Création d'une liste de billets par technos */}
       <ul>
-        <PostLink titre="React" />
-        <PostLink titre="Angular" />
-        <PostLink titre="Vue JS" />
-        <PostLink titre="Svelte" />
-        <PostLink titre="Apprendre Next JS" />
+        <PostLink titre="react" />
+        <PostLink titre="angular" />
+        <PostLink titre="vue" />
+        <PostLink titre="svelte" />
+        <PostLink titre="apprendre Next js" />
       </ul>
+      <h2>Affichage dynamique du titre::::</h2>
+      <h1>{router.query.titre}</h1>
     </Layout>
   )
 }
