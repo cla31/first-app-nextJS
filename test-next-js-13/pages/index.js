a95f0dfd2d43dee8731016cac865ce10cd56717f
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-18 11:17:11 +0200

    Récupération des données avec getServerSideProps

--- test-next-js-13/pages/index.js
+++ test-next-js-13/pages/index.js
@@ -1,16 +1,35 @@
-
 import { Layout } from '@/components/layout'
 import { Inter } from 'next/font/google'
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Home = () => {
+const Home = ({ data }) => {
+  const styles = {
+    padding: 10,
+    margin: 10,
+    borderBottom: '1px solid #DDD',
+  }
   return (
     <Layout>
-         <h1>Bonjour</h1>
+      <h1>Bonjour</h1>
+      {/* {JSON.stringify(data)} */}
+      {data.map((region, index) => (
+        <div key={`${region}-${index}`} style={styles}>
+          <h1>{region.nom}</h1>
+          <p>{region.code}</p>
+        </div>
+      ))}
     </Layout>
-
-   )  
+  )
 }
-
+export async function getServerSideProps(context) {
+  const url = 'https://geo.api.gouv.fr'
+  const { data } = await axios.get(url + '/regions')
+  return {
+    props: {
+      data,
+    },
+  }
+}
 export default Home
